4c63bc59b2c7e16b07929737b3a249a399cd45a8
Ludo <12427684+yumiki@users.noreply.github.com>
2022-11-10 23:46:17 +0100

    Add color picker and update primary color based on it

--- app/src/main/java/com/interview/square/MainActivity.kt
+++ app/src/main/java/com/interview/square/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val colorScheme by themeManager.currentColorScheme.collectAsState()
+            val colorScheme by themeManager.currentColorScheme.collectAsState(themeManager.defaultColorScheme)
             CompositionLocalProvider(
                 LocalThemeManager provides themeManager
             ) {
--- app/src/main/java/com/interview/square/core/domain/repository/IThemeRepository.kt
+++ app/src/main/java/com/interview/square/core/domain/repository/IThemeRepository.kt
@@ -4,12 +4,12 @@ import com.interview.square.core.domain.model.AppColor
 import com.interview.square.core.domain.model.AppColorScheme
 
 interface IThemeRepository {
-    suspend fun updatePrimaryUserColorScheme(color: AppColor)
+    suspend fun updatePrimaryUserColorScheme(color: AppColor, variantColor: AppColor?)
     suspend fun getUserColorScheme(): AppColorScheme
 }
 
 object StubThemeRepository: IThemeRepository {
-    override suspend fun updatePrimaryUserColorScheme(color: AppColor) {
+    override suspend fun updatePrimaryUserColorScheme(color: AppColor, variantColor: AppColor?) {
         TODO("Not yet implemented")
     }
 
