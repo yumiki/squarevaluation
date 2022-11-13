c285cc14edc8ec5945ecbc850a051da46b50d349
Ludo <12427684+yumiki@users.noreply.github.com>
2022-11-13 16:13:57 +0100

    Add preview for history screen

---
+++ app/src/main/java/com/interview/square/feature_position_history/ui/PositionHistoryScreenPreview.kt
@@ -0,0 +1,26 @@
+package com.interview.square.feature_position_history.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.interview.square.core.domain.model.PositionHistory
+import com.interview.square.core.domain.model.TwoDimensionPosition
+import com.interview.square.core.ui.theme.SquareTheme
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import java.util.*
+
+@Preview
+@Composable
+fun PositionHistoryScreenPreview() {
+    SquareTheme {
+        PositionHistoryScreen(object : IPositionHistoryViewModel {
+            override val history: StateFlow<List<PositionHistory>> = MutableStateFlow(
+                List(10) {
+                    PositionHistory(TwoDimensionPosition(it, it),Date().time + it)
+                }
+            )
+        }) {
+
+        }
+    }
+}
